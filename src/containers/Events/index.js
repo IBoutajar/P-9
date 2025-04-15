f9369c4fbcc93118471144fec2c7abc20e670d88
IBoutajar <boutajar2b@hotmail.fr>
2025-04-15 15:01:25 +0200

    Correction bug Catégorie

--- src/containers/Events/index.js
+++ src/containers/Events/index.js
@@ -7,31 +7,38 @@ import ModalEvent from "../ModalEvent";
 
 import "./style.css";
 
-const PER_PAGE = 9;
+const PER_PAGE = 9; // Nombre d'événements par page
 
 const EventList = () => {
-  const { data, error } = useData();
-  const [type, setType] = useState();
-  const [currentPage, setCurrentPage] = useState(1);
-  const filteredEvents = (
-    (!type
-      ? data?.events
-      : data?.events) || []
-  ).filter((event, index) => {
-    if (
-      (currentPage - 1) * PER_PAGE <= index &&
-      PER_PAGE * currentPage > index
-    ) {
-      return true;
-    }
-    return false;
-  });
+  const { data, error } = useData(); // Récupération des données d'événements et des erreurs à partir du contexte
+  const [type, setType] = useState(null); // État pour stocker le type d'événement sélectionné pour le filtrage
+  const [currentPage, setCurrentPage] = useState(1); // État pour stocker le numéro de la page actuelle pour la pagination
+
+  // Fonction appelée lorsqu'un nouveau type d'événement est sélectionné
   const changeType = (evtType) => {
-    setCurrentPage(1);
-    setType(evtType);
+    setCurrentPage(1); // Réinitialisation de la page actuelle à la première page
+    setType(evtType); // Mise à jour du type d'événement sélectionné
   };
-  const pageNumber = Math.floor((filteredEvents?.length || 0) / PER_PAGE) + 1;
+
+  // Création d'un ensemble de types d'événements uniques à partir des données disponibles
   const typeList = new Set(data?.events.map((event) => event.type));
+
+  // Filtrage des événements en fonction du type sélectionné
+  let filteredEvents = data?.events || [];
+  if (type) {
+    filteredEvents = filteredEvents.filter((event) => event.type === type);
+  }
+
+  // Calcul des indices de début et de fin pour la pagination
+  const startIndex = (currentPage - 1) * PER_PAGE;
+  const endIndex = startIndex + PER_PAGE;
+
+  // Sélection des événements à afficher sur la page actuelle
+  const paginatedEvents = filteredEvents.slice(startIndex, endIndex);
+
+  // Calcul du nombre total de pages nécessaires pour paginer tous les événements filtrés
+  const pageNumber = Math.ceil(filteredEvents.length / PER_PAGE);
+
   return (
     <>
       {error && <div>An error occured</div>}
@@ -39,15 +46,20 @@ const EventList = () => {
         "loading"
       ) : (
         <>
+          {/* Sélecteur pour choisir le type d'événement */}
           <h3 className="SelectTitle">Catégories</h3>
           <Select
             selection={Array.from(typeList)}
-            onChange={(value) => (value ? changeType(value) : changeType(null))}
+            onChange={(value) => changeType(value)}
           />
+
+          {/* Conteneur pour afficher les cartes d'événements */}
           <div id="events" className="ListContainer">
-            {filteredEvents.map((event) => (
+            {paginatedEvents.map((event) => (
+              // Modal pour afficher les détails de l'événement lorsqu'il est cliqué
               <Modal key={event.id} Content={<ModalEvent event={event} />}>
                 {({ setIsOpened }) => (
+                  // Carte d'événement affichant les informations de base de l'événement
                   <EventCard
                     onClick={() => setIsOpened(true)}
                     imageSrc={event.cover}
@@ -59,11 +71,16 @@ const EventList = () => {
               </Modal>
             ))}
           </div>
+
+          {/* Pagination pour naviguer entre les pages d'événements */}
           <div className="Pagination">
-            {[...Array(pageNumber || 0)].map((_, n) => (
-              // eslint-disable-next-line react/no-array-index-key
-              <a key={n} href="#events" onClick={() => setCurrentPage(n + 1)}>
-                {n + 1}
+            {[...Array(pageNumber)].map((_, index) => (
+              <a
+                key={`${index.id}`}
+                href="#events"
+                onClick={() => setCurrentPage(index + 1)}
+              >
+                {index + 1}
               </a>
             ))}
           </div>
